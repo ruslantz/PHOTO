deab084edd88fbb48ed397bc8b691c8fe9ece582
ruslantz <91190447+ruslantz@users.noreply.github.com>
2025-10-05 17:04:19 +0300

    Create databases.js

---
+++ databases.js
@@ -0,0 +1,45 @@
+// databases.js
+const databases = {
+    karate: [
+        { id: "GlazovL73", name: "Глазов Лев" },
+        { id: "VlasiukM44", name: "Власюк Максим" },
+        { id: "ShestilovskiiN68", name: "Шестиловский Никита" },
+        { id: "VlaskinP51", name: "Власкин Пётр" },
+        { id: "RudkoN86", name: "Рудько Назар" },
+        { id: "GlushankovM39", name: "Глушанков Матвей" },
+        { id: "SharipovA57", name: "Шарипов Артем" },
+        { id: "VyblyiL62", name: "Выблый Лев" },
+        { id: "MagomedovA91", name: "Магомедов Али" },
+        { id: "TurkinN35", name: "Туркин Николай" },
+        { id: "TurkinK27", name: "Туркин Кирилл" },
+        { id: "SharipovI19", name: "Шарипов Илья" },
+        { id: "BalalaikinaM84", name: "Балалайкина Милана" },
+        { id: "ErshovaI78", name: "Ершова Юлиана" }
+    ],
+
+    engineers: [
+        { id: "AlievaN77", name: "Алиева Николь" },
+        { id: "ZhuravlevaE96", name: "Журавлева Ева" },
+        { id: "IvanushkinM09", name: "Иванушкин Марк" },
+        { id: "KaisinaK55", name: "Кайсина Кира" },
+        { id: "KapitonovM17", name: "Капитонов Михаил" },
+        { id: "OstapchukA15", name: "Остапчук Арсений" },
+        { id: "SalyakinA30", name: "Салякин Алексей" },
+        { id: "ShilinM81", name: "Шилин Максим" },
+        { id: "AnaevB73", name: "Анаев Борис" },
+        { id: "BaranovD88", name: "Баранов Даниил" },
+        { id: "DjahayaS42", name: "Джахая Сандро" },
+        { id: "ZhuravlevaA51", name: "Журавлева Аврора" },
+        { id: "KazaryanA12", name: "Казарян Артемий" },
+        { id: "KuznetsovA68", name: "Кузнецов Арсений" },
+        { id: "PantileykoA24", name: "Пантилейко Артем" },
+        { id: "RutskiyY33", name: "Руцкий Ян" },
+        { id: "StepanenkoM65", name: "Степаненко Мия" },
+        { id: "ChzhouS77", name: "Чжоу Шуянь" },
+        { id: "SavinovaE32", name: "Савинова Елизавета" },
+        { id: "ZicinL25", name: "Цыцин Лука" },
+        { id: "SavinovI64", name: "Савинов Илья" },
+        { id: "DanilinaV17", name: "Данилина Виктория" },
+        { id: "SidorovF63", name: "Сидоров Фёдор" }
+    ]
+};
